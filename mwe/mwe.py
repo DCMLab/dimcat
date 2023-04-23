2a2496331501a7eb5612581cdd9678cb52724760
johentsch <johannes.hentschel@epfl.ch>
2023-04-24 00:51:52 +0200

    adds minimal working example

---
+++ mwe/mwe.py
@@ -0,0 +1,17 @@
+from dimcat.analyzers import CounterConfig
+from dimcat.base import DimcatObject
+from dimcat.dataset import Dataset
+from dimcat.features import FeatureName
+
+if __name__ == "__main__":
+    dataset = Dataset()
+    dataset.load_package("dcml_corpora.datapackage.json")
+
+    selected_feature = FeatureName("KeyAnnotations")
+    analyzer_config = CounterConfig(feature_config=selected_feature)
+    analyzer = DimcatObject.from_config(analyzer_config)
+    analyzed = analyzer.process(dataset)
+
+    result = analyzed.result
+    fig = result.plot()
+    print(fig)  # fig.show()
