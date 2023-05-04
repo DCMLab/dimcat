88f1f0ef103c721940d07592fd6f9b594e6a6423
johentsch <johannes.hentschel@epfl.ch>
2023-05-04 16:23:46 +0200

    enhances features.NotesConfig

--- src/dimcat/features/notes.py
+++ src/dimcat/features/notes.py
@@ -1,11 +1,22 @@
 from dataclasses import dataclass
+from enum import Enum
 
 from dimcat.features.base import Feature, FeatureConfig, FeatureName
+
+
+class NotesFormat(str, Enum):
+    NAME = "NAME"
+    FIFTHS = "FIFTHS"
+    MIDI = "MIDI"
+    DEGREE = "DEGREE"
+    INTERVAL = "INTERVAL"
 
 
 @dataclass(frozen=True)
 class NotesConfig(FeatureConfig):
     _configured_class = FeatureName.Notes
+    format: NotesFormat = NotesFormat.NAME
+    weight_grace_notes: float = 0.0
 
 
 class Notes(Feature):
