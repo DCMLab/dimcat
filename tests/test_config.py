85e93464477519376ad5ddecb0ab4c2b118bdd24
johentsch <johannes.hentschel@epfl.ch>
2023-05-15 13:34:37 +0200

    extends marshmallow proof-of-concept with a DimcatResource to be serialized via frictionless

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,13 +3,19 @@ from __future__ import annotations
 import json
 from itertools import product
 from pprint import pprint
+from typing import Optional, Union
+from zipfile import ZipFile
 
+import frictionless as fl
+import ms3
+import pandas as pd
 import pytest
-from dimcat.base import DimcatObject
+from dimcat.base import DimcatObject, WrappedDataframe
 from dimcat.config import DimcatConfig, DimcatSchema
 from dimcat.utils import get_schema
 from marshmallow import ValidationError, fields
 from marshmallow.class_registry import _registry as MM_REGISTRY
+from typing_extensions import Self
 
 
 def obj_from_dict(obj_data):
@@ -38,17 +44,105 @@ class BaseObject(DimcatObject):
     class Schema(DimcatSchema):
         strong = fields.String(required=True)
 
-    def __init__(self, strong: str):
+    def __init__(self, **kwargs):
         self.schema = get_schema(
             self.name
         )  # each object gets the same, cached instance
-        self.strong = strong
 
     def to_dict(self) -> dict:
         return DimcatConfig.from_object(self)
 
     def to_json(self) -> dict:
         return self.schema.dumps(self)
+
+
+class DimcatResource(BaseObject):
+    class Schema(DimcatSchema):
+        resource = fields.Method(
+            serialize="get_descriptor", deserialize="load_descriptor"
+        )
+
+        def get_descriptor(self, dc_resource: DimcatResource):
+            if dc_resource.path is None:
+                raise ValidationError(
+                    f"Cannot serialize this {dc_resource.name} because it doesn't have a file path."
+                )
+            descriptor = dc_resource.resource.to_descriptor()
+            return descriptor
+
+        def load_descriptor(self, descriptor):
+            return fl.Resource.from_descriptor(descriptor)
+
+    def __init__(self, resource: Optional[fl.Resource] = None) -> None:
+        super().__init__()
+        self.resource: fl.Resource = fl.Resource()
+        if resource is not None:
+            if isinstance(resource, fl.Resource):
+                self.resource = resource
+            else:
+                obj_name = self.__name__
+                r_type = type(resource)
+                msg = f"{obj_name} takes a frictionless.Resource, not {r_type}."
+                if issubclass(r_type, str):
+                    msg += f" Try {obj_name}.from_descriptor()"
+                raise ValueError(msg)
+
+    @property
+    def path(self):
+        return self.resource.path
+
+    @path.setter
+    def path(self, new_path):
+        self.resource.path = new_path
+
+    @classmethod
+    def from_dataframe(cls, df, **kwargs):
+        resource = fl.describe(df, **kwargs)
+        # fl.validate(resource)
+        return cls(resource=resource)
+
+    @classmethod
+    def from_descriptor(
+        cls, descriptor: Union[fl.interfaces.IDescriptor, str], **options
+    ) -> Self:
+        resource = fl.Resource.from_descriptor(descriptor, **options)
+        return cls(resource=resource)
+
+    def __str__(self):
+        return str(self.resource)
+
+    def __repr__(self):
+        return repr(self.resource)
+
+    def get_pandas(
+        self, wrapped=True
+    ) -> Union[WrappedDataframe[pd.DataFrame], pd.DataFrame]:
+        r = self.resource
+        if r.path is None:
+            raise ValidationError(
+                "The resource does not refer to a file path and cannot be restored."
+            )
+        s = r.schema
+        if r.normpath.endswith(".zip") or r.compression == "zip":
+            zip_file_handler = ZipFile(r.normpath)
+            df = ms3.load_tsv(zip_file_handler.open(r.innerpath))
+        else:
+            raise NotImplementedError()
+        if len(s.primary_key) > 0:
+            df = df.set_index(s.primary_key)
+        if wrapped:
+            return WrappedDataframe.from_df(df)
+        return df
+
+
+def test_dc_resource():
+    df = ms3.load_tsv("~/dcml_corpora/tchaikovsky_seasons/harmonies/op37a10.tsv")
+    resource = DimcatResource.from_dataframe(df=df)
+    serialized = resource.to_dict()
+    print(serialized)
+    deserialized = obj_from_dict(serialized)
+    restored_df = deserialized.get_pandas()
+    print(restored_df)
 
 
 class SubClass(BaseObject):
