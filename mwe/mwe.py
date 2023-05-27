d853df5f5fdaa8239b1a816ea6b5a8ca2a3cfcc4
johentsch <johannes.hentschel@epfl.ch>
2023-05-27 15:57:42 +0200

    adapts minimal working example to new architecture

--- mwe/mwe.py
+++ mwe/mwe.py
@@ -1,48 +1,27 @@
+import os
+
+from dimcat import Dataset, DimcatConfig
 from dimcat.analyzers import Counter
-from dimcat.base import DimcatConfig
-from dimcat.dataset import Dataset
-from dimcat.features import FeatureName
+from dimcat.resources.features import FeatureName
 
 if __name__ == "__main__":
+    os.chdir(os.path.dirname(__file__))
     dataset = Dataset()
     dataset.load_package("dcml_corpora.datapackage.json")
-    # any of these work as argument for "feature_config"
-    feature_name = "nOtEs"  # arbitrary case
-    feature_enum = FeatureName(feature_name)
-    feature_constructor = feature_enum.get_class()
-    F = feature_constructor()
-    feature_dict = F.to_dict()
-    default_config1 = F.to_config()
+    feature_enum = FeatureName("nOtEs")
+
     minimal_config = DimcatConfig(dtype=feature_enum)
-    report = default_config1.options_schema.validate(minimal_config)
-    assert not report
-    report = minimal_config.options_schema.validate(default_config1)
-    assert not report
     weighted_config = DimcatConfig(minimal_config, weight_grace_notes=0.5)
-    assert type(minimal_config) == type(
-        weighted_config
-    ), f"{type(minimal_config)} != {type(weighted_config)}"
 
-    analyzer0 = Counter(features=weighted_config)
-    analyzer_config0 = analyzer0.to_config()
-    analyzer1 = analyzer_config0.create()
-    analyzer_dict1 = analyzer1.to_dict()
-    assert analyzer0.to_dict() == analyzer1.to_dict()
-    assert analyzer_config0 == analyzer_dict1
+    Notes_constructor = feature_enum.get_class()
+    default_config = Notes_constructor().to_config()
 
-    json_str = analyzer1.to_json()
-    analyzer_config1 = DimcatConfig.from_json(json_str)
-    analyzer2 = analyzer_config1.create()
-    assert analyzer1.to_dict() == analyzer2.to_dict()
-    assert analyzer_config0 == analyzer_config1
+    analyzer_config = DimcatConfig(dtype="Counter", features=weighted_config)
+    analyzer1 = analyzer_config.create()
+    analyzer2 = Counter(features=weighted_config)
+    assert analyzer1 == analyzer2, f"{analyzer1.to_dict()} != {analyzer2.to_dict()}"
 
-    json_path = "counter.json"
-    analyzer2.to_json_file(json_path)
-    analyzer3 = DimcatConfig.from_json_file(json_path).create()
-    assert analyzer2.to_dict() == analyzer3.to_dict()
-
-    analyzer_config3 = analyzer3.to_config()
-    analyzer4 = Counter.from_config(analyzer_config3)
-    assert analyzer3.to_dict() == analyzer4.to_dict()
-
-    analyzed = analyzer4.process(dataset)
+    analyzed = analyzer1.process(dataset)
+    result = analyzed.get_result()
+    fig = result.plot()
+    print(fig)
