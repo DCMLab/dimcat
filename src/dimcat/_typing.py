b786d864c494abafa1fe50559291f8c95a223041
johentsch <johannes.hentschel@epfl.ch>
2023-05-30 22:57:39 +0200

    removing modules belonging to the DiMCAT prototype version (RIP)

--- src/dimcat/_typing.py
+++
@@ -1,7 +0,0 @@
-from typing import Tuple, TypeAlias, Union
-
-import pandas as pd
-
-Pandas: TypeAlias = Union[pd.Series, pd.DataFrame]
-GroupID: TypeAlias = tuple
-Index: TypeAlias = Union[Tuple[str, str], Tuple[str, str, pd.Interval]]
