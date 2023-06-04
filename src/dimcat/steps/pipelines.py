05dd8e8976150bb4348678e41e8b742cdfae602e
johentsch <johannes.hentschel@epfl.ch>
2023-06-04 21:35:06 +0200

    gives the Pipeline its own processing logic and adds test_pipeline.py

--- src/dimcat/steps/pipelines.py
+++ src/dimcat/steps/pipelines.py
@@ -4,6 +4,8 @@ import logging
 from typing import Iterable, Iterator, List, Optional
 
 from dimcat.base import DimcatConfig, DimcatObject, DimcatObjectField
+from dimcat.data.dataset import Dataset
+from dimcat.data.resources import DimcatResource, FeatureSpecs
 from marshmallow import fields
 
 from .base import PipelineStep
@@ -24,18 +26,24 @@ class Pipeline(PipelineStep):
         steps: Optional[
             PipelineStep | DimcatConfig | Iterable[PipelineStep | DimcatConfig]
         ] = None,
+        features: Optional[FeatureSpecs | Iterable[FeatureSpecs]] = None,
         **kwargs,
     ):
+        super().__init__(features=features, **kwargs)
         self._steps: List[PipelineStep] = []
         if steps is not None:
             self.steps = steps
 
     def __iter__(self) -> Iterator[PipelineStep]:
-        yield from self.steps
+        yield from self._steps
+
+    def __len__(self):
+        return len(self._steps)
 
     @property
     def steps(self) -> List[PipelineStep]:
-        return self._steps
+        """The pipeline steps as a list. Modifying the list does not affect the pipeline."""
+        return list(self._steps)
 
     @steps.setter
     def steps(
@@ -58,3 +66,25 @@ class Pipeline(PipelineStep):
         if not isinstance(step, PipelineStep):
             raise TypeError(f"Pipeline acceppts only PipelineSteps, not {type(step)}.")
         self._steps.append(step)
+
+    def _process_dataset(self, dataset: Dataset) -> Dataset:
+        if len(self._steps) == 0:
+            self.logger.info("Nothing to do.")
+            return dataset
+        previous_dataset = dataset
+        for step in self._steps:
+            processed_dataset = step.process(previous_dataset)
+            # ToDo: checks?
+            previous_dataset = processed_dataset
+        return processed_dataset
+
+    def _process_resource(self, resource: DimcatResource) -> DimcatResource:
+        if len(self._steps) == 0:
+            self.logger.info("Nothing to do.")
+            return resource
+        previous_resource = resource
+        for step in self._steps:
+            processed_resource = step.dispatch(previous_resource)
+            # ToDo: checks?
+            previous_resource = processed_resource
+        return processed_resource
