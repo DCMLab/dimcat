4723ce4b1539ce3d48837c6127d387b5a880dbd0
johentsch <johannes.hentschel@epfl.ch>
2023-06-04 11:37:40 +0200

    improved DimcatError mechanism accepting arguments

--- src/dimcat/exceptions.py
+++ src/dimcat/exceptions.py
@@ -1,34 +1,83 @@
-class DimcatError(Exception):
-    default_message: str = "Something went wrong."
+import logging
+from typing import Callable, ClassVar, Dict, Optional
 
-    def __init__(self, *args, **kwargs):
-        """Following frnkstns solution via https://stackoverflow.com/a/49224771"""
-        if not args:
-            args = (self.default_message,)
+logger = logging.getLogger(__name__)
+
+
+class DimcatError(Exception):
+    nargs2message: ClassVar[Dict[str, str | Callable]] = {0: "Something went wrong."}
+    """Mapping the number of arguments passed to the Error to a lambda string constructor accepting that number of
+    arguments and generates the error message."""
+
+    def __init__(self, *args, message: Optional[str] = None, **kwargs):
+        """Constructor for DimcatError. Pass message="My custom message" to override the default message.
+
+        Args:
+            *args:
+                If the number of args matches a string constructor in self.nargs2message, that constructor is called.
+                Otherwise, a single argument is used as custom message. If no args a given, the default message is used.
+            message: Pass message="My custom message" to override the default message and mechanism based on args.
+            **kwargs:
+        """
+        if message is not None:
+            msg = message
+        else:
+            nargs = len(args)
+            if nargs in self.nargs2message:
+                msg = self.nargs2message[nargs]
+                if not isinstance(msg, str):
+                    msg = msg(*args)
+            else:
+                msg = self.nargs2message[0]
+        args = (msg,)
 
         # Call super constructor
-        super().__init__(*args, **kwargs)
+        super().__init__(*args)
 
 
 class EmptyDatasetError(DimcatError):
-    default_message = "The dataset is empty."
+    """optional args: (dataset_name,)"""
+
+    nargs2message = {
+        0: "The dataset is empty.",
+        1: lambda name: f"Dataset {name!r} is empty.",
+    }
 
 
 class EmptyResourceError(DimcatError):
-    default_message = "The resource is empty."
+    """optional args: (resource_name,)"""
+
+    nargs2message = {
+        0: "The resource is empty.",
+        1: lambda name: f"Resource {name!r} is empty.",
+    }
 
 
 class EmptyPackageError(DimcatError):
-    default_message = "The package is empty."
+    """optional args: (package_name,)"""
+
+    nargs2message = {
+        0: "The package is empty.",
+        1: lambda name: f"Package {name!r} is empty.",
+    }
 
 
 class FeatureUnavailableError(DimcatError):
-    default_message = "A required feature is not available."
+    """optional args: (feature_name,)"""
+
+    nargs2message = {
+        0: "A required feature is not available.",
+        1: lambda name: f"Feature {name!r} is not available.",
+    }
 
 
 class NoFeaturesSelectedError(DimcatError):
-    default_message = "No features selected."
+    nargs2message = {
+        0: "No features have been selected.",
+    }
 
 
 class BasePathNotDefinedError(DimcatError):
-    default_message = "The basepath is not defined."
+    nargs2message = {
+        0: "The base path is not defined.",
+    }
