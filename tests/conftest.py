2e5e125b719ddd86df11d537a71e43d002e5d982
johentsch <johannes.hentschel@epfl.ch>
2023-06-09 19:14:36 +0200

    has the GitHub action "checkout" also clone submodules recursively

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,9 +28,9 @@ def pytest_terminal_summary(terminalreporter: TerminalReporter, exitstatus, conf
 # ----------------------------- SETTINGS -----------------------------
 # Directory holding your clone of github.com/DCMLab/unittest_metacorpus
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
-print(f"TEST_DIR: {TEST_DIR!r}. Contents: {os.listdir(TEST_DIR)}")
+logger.debug(f"TEST_DIR: {TEST_DIR!r}. Contents: {os.listdir(TEST_DIR)}")
 CORPUS_DIR = os.path.abspath(os.path.join(TEST_DIR, ".."))
-print(f"CORPUS_DIR: {CORPUS_DIR!r}. Contents: {os.listdir(CORPUS_DIR)}")
+logger.debug(f"CORPUS_DIR: {CORPUS_DIR!r}. Contents: {os.listdir(CORPUS_DIR)}")
 
 # region test directories and files
 
