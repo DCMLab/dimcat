7ef34c8d34db9a66f9535ce34cfd1c321ce3ecf3
Johannes Hentschel <johannes.hentschel@epfl.ch>
2023-11-03 02:12:53 +0100

    reorganizes grouper modules, adds general ColumnGrouper, makes ModeGrouper a special case of it, adds MeasureGrouper

--- src/dimcat/steps/groupers/__init__.py
+++ src/dimcat/steps/groupers/__init__.py
@@ -1,6 +1,7 @@
 import logging
 
-from .base import CorpusGrouper, CustomPieceGrouper, YearGrouper
-from .keys import ModeGrouper
+from .base import CorpusGrouper, CustomPieceGrouper
+from .columns import ColumnGrouper, MeasureGrouper, ModeGrouper
+from .metadata import YearGrouper
 
 logger = logging.getLogger(__name__)
--- src/dimcat/steps/groupers/base.py
+++ src/dimcat/steps/groupers/base.py
@@ -1,6 +1,5 @@
 import logging
 from collections import defaultdict
-from numbers import Number
 from typing import (
     Dict,
     Hashable,
@@ -21,7 +20,7 @@ from dimcat.data.resources.dc import DimcatIndex, DimcatResource, PieceIndex
 from dimcat.data.resources.features import Feature
 from dimcat.dc_exceptions import GrouperNotSetUpError, ResourceNotProcessableError
 from dimcat.steps.base import FeatureProcessingStep
-from dimcat.utils import check_name, get_middle_composition_year
+from dimcat.utils import check_name
 from typing_extensions import Self
 
 logger = logging.getLogger(__name__)
@@ -203,48 +202,3 @@ class CustomPieceGrouper(Grouper):
         if len(self.grouped_pieces) == 0:
             raise GrouperNotSetUpError(self.dtype)
         super().check_resource(resource)
-
-
-class YearGrouper(CustomPieceGrouper):
-    @classmethod
-    def from_grouping(
-        cls,
-        piece_groups: Dict[Number, List[tuple]],
-        level_names: Sequence[str] = ("middle_composition_year", "corpus", "piece"),
-        sort: bool = False,
-        raise_if_multiple_membership: bool = False,
-    ) -> Self:
-        """Creates a YearGrouper from a dictionary of piece groups.
-
-        Args:
-        grouping: A dictionary where keys are group names and values are lists of index tuples.
-        level_names:
-            Names for the levels of the MultiIndex, i.e. one for the group level and one per level in the tuples.
-        sort: By default the returned MultiIndex is not sorted. Set True to disable sorting.
-        raise_if_multiple_membership: If True, raises a ValueError if a member is in multiple groups.
-        """
-        return super().from_grouping(
-            piece_groups=piece_groups,
-            level_names=level_names,
-            sort=sort,
-            raise_if_multiple_membership=raise_if_multiple_membership,
-        )
-
-    def __init__(
-        self,
-        level_name: str = "middle_composition_year",
-        grouped_pieces: DimcatIndex | pd.MultiIndex = None,
-        **kwargs,
-    ):
-        super().__init__(level_name=level_name, grouped_pieces=grouped_pieces, **kwargs)
-
-    def fit_to_dataset(self, dataset: Dataset) -> None:
-        metadata = dataset.get_metadata()
-        sorted_composition_years = get_middle_composition_year(metadata).sort_values()
-        grouping = sorted_composition_years.groupby(
-            sorted_composition_years, sort=True
-        ).groups
-        group_index = DimcatIndex.from_grouping(
-            grouping, ("middle_composition_year", "corpus", "piece")
-        )
-        self.grouped_pieces = group_index
