73595055e32ce7b1d035895544f2b9dd6897ff83
Johannes Hentschel <johannes.hentschel@epfl.ch>
2023-11-23 17:56:05 +0100

    sorts functions in dimcat.utils alphabetically

--- src/dimcat/utils.py
+++ src/dimcat/utils.py
@@ -24,6 +24,8 @@ from dimcat.base import FriendlyEnum
 from dimcat.data.base import AbsolutePathStr
 from tqdm.auto import tqdm
 
+# region constants
+
 logger = logging.getLogger(__name__)
 
 FRICTIONLESS_NAME_PATTERN = (
@@ -31,37 +33,119 @@ FRICTIONLESS_NAME_PATTERN = (
 )
 FRICTIONLESS_INVERSE = r"[^-a-z0-9._/]"
 
-
-def make_valid_frictionless_name(name: str, replace_char="_") -> str:
-    if not isinstance(name, str):
-        raise TypeError(f"Name must be a string, not {type(name)}")
-    name = name.lower()
-    if not re.match(FRICTIONLESS_NAME_PATTERN, name):
-        name = re.sub(FRICTIONLESS_INVERSE, replace_char, name)
-    return name
+# endregion constants
+# region classes
 
 
-def make_valid_frictionless_name_from_filepath(
-    path: str, include_extension=True, replace_char="_"
+class SortOrder(FriendlyEnum):
+    ASCENDING = "ASCENDING"
+    DESCENDING = "DESCENDING"
+    NONE = "NONE"
+
+
+# endregion classes
+# region functions
+
+
+def check_file_path(
+    filepath: str,
+    extensions: Optional[str | Collection[str]] = None,
+    must_exist: bool = True,
 ) -> str:
-    if include_extension:
-        return make_valid_frictionless_name(path, replace_char=replace_char)
-    name, _ = os.path.splitext(path)
-    return make_valid_frictionless_name(name, replace_char=replace_char)
+    """Checks that the filepath exists and raises an exception otherwise (or if it doesn't have a valid extension).
+
+    Args:
+        filepath:
+        extensions:
+        must_exist: If True (default), raises FileNotFoundError if the file does not exist.
+
+    Returns:
+        The path turned into an absolute path.
+
+    Raises:
+        FileNotFoundError: If the file does not exist and must_exist is True.
+        ValueError: If the file does not have one of the specified extensions, if any.
+    """
+    path = resolve_path(filepath)
+    if must_exist and not os.path.isfile(path):
+        raise FileNotFoundError(f"File {path} does not exist.")
+    if extensions is not None:
+        if isinstance(extensions, str):
+            extensions = [extensions]
+        if not any(path.endswith(ext) for ext in extensions):
+            plural = f"one of {extensions}" if len(extensions) > 1 else extensions[0]
+            _, file_ext = os.path.splitext(path)
+            raise ValueError(f"File {path} has extension {file_ext}, not {plural}.")
+    return path
 
 
-def nest_level(obj, include_tuples=False):
-    """Recursively calculate the depth of a nested list."""
-    if obj.__class__ != list:
-        if include_tuples:
-            if obj.__class__ != tuple:
-                return 0
-        else:
-            return 0
-    max_level = 0
-    for item in obj:
-        max_level = max(max_level, nest_level(item, include_tuples=include_tuples))
-    return max_level + 1
+def check_name(name: str) -> None:
+    """Check if a name is valid according to frictionless.
+
+    Raises:
+        ValueError: If the name is not valid.
+    """
+    if not re.match(FRICTIONLESS_NAME_PATTERN, name):
+        raise ValueError(
+            f"Name can only contain [a-z], [0-9], [-._/], and no spaces: {name!r}"
+        )
+
+
+def clean_index_levels(pandas_obj):
+    """Remove index levels "IDs", "corpus" and "fname", if redundant."""
+    idx = pandas_obj.index
+    drop = []
+    if idx.nlevels > 1 and "IDs" in idx.names:
+        drop.append("IDs")
+    if idx.names.count("corpus") > 1:
+        drop.append("corpus")
+    if idx.names.count("fname") > 1:
+        drop.append("fname")
+    if len(drop) > 0:
+        # for each name, store the integer of the last level with that name
+        name2level = {name: level for level, name in enumerate(idx.names)}
+        drop_levels = [name2level[name] for name in drop]
+        return pandas_obj.droplevel(drop_levels)
+    return pandas_obj
+
+
+def get_composition_year(metadata_dict):
+    """The logic for getting a composition year out of the given metadata dictionary."""
+    start = (
+        metadata_dict["composed_start"] if "composed_start" in metadata_dict else None
+    )
+    end = metadata_dict["composed_end"] if "composed_end" in metadata_dict else None
+    if pd.isnull(start) and pd.isnull(end):
+        raise LookupError("Metadata do not include composition dates.")
+    if pd.isnull(start):
+        return end
+    if pd.isnull(end):
+        return start
+    return round((end + start) / 2, ndigits=1)
+
+
+def get_middle_composition_year(
+    metadata: pd.DataFrame,
+    composed_start_column: str = "composed_start",
+    composed_end_column: str = "composed_end",
+) -> pd.Series:
+    """Returns the middle of the composition year range."""
+    composed_start = pd.to_numeric(metadata[composed_start_column], errors="coerce")
+    composed_end = pd.to_numeric(metadata[composed_end_column], errors="coerce")
+    composed_start.fillna(composed_end, inplace=True)
+    composed_end.fillna(composed_start, inplace=True)
+    return (composed_start + composed_end) / 2
+
+
+def get_object_value(obj, key, default):
+    """Return obj[key] if possible, obj.key otherwise. Code copied from marshmallow.utils._get_value_for_key()"""
+    if not hasattr(obj, "__getitem__"):
+        return getattr(obj, key, default)
+
+    try:
+        return obj[key]
+    except (KeyError, IndexError, TypeError, AttributeError):
+        return getattr(obj, key, default)
 
 
 def grams(lists_of_symbols, n=2, to_string: bool = False):
@@ -93,37 +177,39 @@ def grams(lists_of_symbols, n=2, to_string: bool = False):
             return ngrams
 
 
-def get_composition_year(metadata_dict):
-    """The logic for getting a composition year out of the given metadata dictionary."""
-    start = (
-        metadata_dict["composed_start"] if "composed_start" in metadata_dict else None
-    )
-    end = metadata_dict["composed_end"] if "composed_end" in metadata_dict else None
-    if pd.isnull(start) and pd.isnull(end):
-        raise LookupError("Metadata do not include composition dates.")
-    if pd.isnull(start):
-        return end
-    if pd.isnull(end):
-        return start
-    return round((end + start) / 2, ndigits=1)
+def interval_index2interval(ix):
+    """Takes an interval index and returns the interval corresponding to [min(left), max(right))."""
+    left = ix.left.min()
+    right = ix.right.max()
+    return pd.Interval(left, right, closed="left")
 
 
-def clean_index_levels(pandas_obj):
-    """Remove index levels "IDs", "corpus" and "fname", if redundant."""
-    idx = pandas_obj.index
-    drop = []
-    if idx.nlevels > 1 and "IDs" in idx.names:
-        drop.append("IDs")
-    if idx.names.count("corpus") > 1:
-        drop.append("corpus")
-    if idx.names.count("fname") > 1:
-        drop.append("fname")
-    if len(drop) > 0:
-        # for each name, store the integer of the last level with that name
-        name2level = {name: level for level, name in enumerate(idx.names)}
-        drop_levels = [name2level[name] for name in drop]
-        return pandas_obj.droplevel(drop_levels)
-    return pandas_obj
+def is_uri(path: str) -> bool:
+    """Solution from https://stackoverflow.com/a/38020041"""
+    try:
+        result = urlparse(path)
+        return all([result.scheme, result.netloc])
+    except Exception:
+        return False
+
+
+def make_extension_regex(
+    extensions: Iterable[str],
+    enforce_initial_dot: bool = False,
+) -> re.Pattern:
+    """Turns file extensions into a regular expression."""
+    if not extensions:
+        return re.compile(".*")
+    if isinstance(extensions, str):
+        extensions = [extensions]
+    else:
+        extensions = list(extensions)
+    if enforce_initial_dot:
+        dot = r"\."
+        regex = f"(?:{'|'.join(dot + e.lstrip('.') for e in extensions)})$"
+    else:
+        regex = f"(?:{'|'.join(extensions)})$"
+    return re.compile(regex, re.IGNORECASE)
 
 
 def make_suffix(*params):
@@ -279,11 +365,36 @@ def make_transition_matrix(
     return df
 
 
-def interval_index2interval(ix):
-    """Takes an interval index and returns the interval corresponding to [min(left), max(right))."""
-    left = ix.left.min()
-    right = ix.right.max()
-    return pd.Interval(left, right, closed="left")
+def make_valid_frictionless_name(name: str, replace_char="_") -> str:
+    if not isinstance(name, str):
+        raise TypeError(f"Name must be a string, not {type(name)}")
+    name = name.lower()
+    if not re.match(FRICTIONLESS_NAME_PATTERN, name):
+        name = re.sub(FRICTIONLESS_INVERSE, replace_char, name)
+    return name
+
+
+def make_valid_frictionless_name_from_filepath(
+    path: str, include_extension=True, replace_char="_"
+) -> str:
+    if include_extension:
+        return make_valid_frictionless_name(path, replace_char=replace_char)
+    name, _ = os.path.splitext(path)
+    return make_valid_frictionless_name(name, replace_char=replace_char)
+
+
+def nest_level(obj, include_tuples=False):
+    """Recursively calculate the depth of a nested list."""
+    if obj.__class__ != list:
+        if include_tuples:
+            if obj.__class__ != tuple:
+                return 0
+        else:
+            return 0
+    max_level = 0
+    for item in obj:
+        max_level = max(max_level, nest_level(item, include_tuples=include_tuples))
+    return max_level + 1
 
 
 def replace_ext(filepath, new_ext):
@@ -296,76 +407,17 @@ def replace_ext(filepath, new_ext):
     return file + new_ext
 
 
-def get_object_value(obj, key, default):
-    """Return obj[key] if possible, obj.key otherwise. Code copied from marshmallow.utils._get_value_for_key()"""
-    if not hasattr(obj, "__getitem__"):
-        return getattr(obj, key, default)
-
-    try:
-        return obj[key]
-    except (KeyError, IndexError, TypeError, AttributeError):
-        return getattr(obj, key, default)
+@overload
+def resolve_path(path: str) -> AbsolutePathStr:
+    ...
 
 
-def check_file_path(
-    filepath: str,
-    extensions: Optional[str | Collection[str]] = None,
-    must_exist: bool = True,
-) -> str:
-    """Checks that the filepath exists and raises an exception otherwise (or if it doesn't have a valid extension).
-
-    Args:
-        filepath:
-        extensions:
-        must_exist: If True (default), raises FileNotFoundError if the file does not exist.
-
-    Returns:
-        The path turned into an absolute path.
-
-    Raises:
-        FileNotFoundError: If the file does not exist and must_exist is True.
-        ValueError: If the file does not have one of the specified extensions, if any.
-    """
-    path = resolve_path(filepath)
-    if must_exist and not os.path.isfile(path):
-        raise FileNotFoundError(f"File {path} does not exist.")
-    if extensions is not None:
-        if isinstance(extensions, str):
-            extensions = [extensions]
-        if not any(path.endswith(ext) for ext in extensions):
-            plural = f"one of {extensions}" if len(extensions) > 1 else extensions[0]
-            _, file_ext = os.path.splitext(path)
-            raise ValueError(f"File {path} has extension {file_ext}, not {plural}.")
-    return path
+@overload
+def resolve_path(path: Literal[None]) -> None:
+    ...
 
 
-def check_name(name: str) -> None:
-    """Check if a name is valid according to frictionless.
-
-    Raises:
-        ValueError: If the name is not valid.
-    """
-    if not re.match(FRICTIONLESS_NAME_PATTERN, name):
-        raise ValueError(
-            f"Name can only contain [a-z], [0-9], [-._/], and no spaces: {name!r}"
-        )
-
-
-def _set_new_basepath(basepath: str, other_logger=None) -> None:
-    if basepath is None:
-        return
-    basepath_arg = resolve_path(basepath)
-    if not os.path.isdir(basepath_arg):
-        raise NotADirectoryError(
-            f"basepath {basepath_arg!r} is not an existing directory."
-        )
-    if other_logger is None:
-        other_logger = logger
-    other_logger.debug(f"The basepath been set to {basepath_arg!r}")
-    return basepath_arg
-
-
-def resolve_path(path) -> Optional[AbsolutePathStr]:
+def resolve_path(path: str | Literal[None]) -> Optional[AbsolutePathStr]:
     """Resolves '~' to HOME directory and turns ``path`` into an absolute path."""
     if path is None:
         return None
@@ -383,13 +435,35 @@ def resolve_path(path) -> Optional[AbsolutePathStr]:
     return AbsolutePathStr(path)
 
 
-def is_uri(path: str) -> bool:
-    """Solution from https://stackoverflow.com/a/38020041"""
-    try:
-        result = urlparse(path)
-        return all([result.scheme, result.netloc])
-    except Exception:
-        return False
+@overload
+def treat_basepath_argument(path: str, other_logger) -> AbsolutePathStr:
+    ...
+
+
+@overload
+def treat_basepath_argument(path: Literal[None], other_logger) -> None:
+    ...
+
+
+def treat_basepath_argument(
+    basepath: str | Literal[None], other_logger: Optional[logging.Logger] = None
+) -> Optional[AbsolutePathStr]:
+    """Turns ``basepath`` into an absolute path and checks that it exists.
+
+    Raises:
+        NotADirectoryError: If ``basepath`` is not an existing directory.
+    """
+    if basepath is None:
+        return
+    basepath_arg = resolve_path(basepath)
+    if not os.path.isdir(basepath_arg):
+        raise NotADirectoryError(
+            f"basepath {basepath_arg!r} is not an existing directory."
+        )
+    if other_logger is None:
+        other_logger = logger
+    other_logger.debug(f"The basepath been set to {basepath_arg!r}")
+    return basepath_arg
 
 
 @overload
@@ -511,39 +585,4 @@ def scan_directory(
     return traverse(directory)
 
 
-def make_extension_regex(
-    extensions: Iterable[str],
-    enforce_initial_dot: bool = False,
-) -> re.Pattern:
-    """Turns file extensions into a regular expression."""
-    if not extensions:
-        return re.compile(".*")
-    if isinstance(extensions, str):
-        extensions = [extensions]
-    else:
-        extensions = list(extensions)
-    if enforce_initial_dot:
-        dot = r"\."
-        regex = f"(?:{'|'.join(dot + e.lstrip('.') for e in extensions)})$"
-    else:
-        regex = f"(?:{'|'.join(extensions)})$"
-    return re.compile(regex, re.IGNORECASE)
-
-
-def get_middle_composition_year(
-    metadata: pd.DataFrame,
-    composed_start_column: str = "composed_start",
-    composed_end_column: str = "composed_end",
-) -> pd.Series:
-    """Returns the middle of the composition year range."""
-    composed_start = pd.to_numeric(metadata[composed_start_column], errors="coerce")
-    composed_end = pd.to_numeric(metadata[composed_end_column], errors="coerce")
-    composed_start.fillna(composed_end, inplace=True)
-    composed_end.fillna(composed_start, inplace=True)
-    return (composed_start + composed_end) / 2
-
-
-class SortOrder(FriendlyEnum):
-    ASCENDING = "ASCENDING"
-    DESCENDING = "DESCENDING"
-    NONE = "NONE"
+# endregion functions
