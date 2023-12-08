e51625fd6b7fd004ff1eb7afeb93139f5d728adc
Johannes Hentschel <johannes.hentschel@epfl.ch>
2023-12-08 17:18:10 +0100

    fix: align_with_grouping() did not work for NgramTables because pandas prevents merge with diverging column nlevels, even if one of the sides has no columns

--- src/dimcat/data/resources/utils.py
+++ src/dimcat/data/resources/utils.py
@@ -76,11 +76,16 @@ def align_with_grouping(
     shared_levels = set(df_levels).intersection(set(gr_levels))
     if len(shared_levels) == 0:
         raise ValueError(f"No shared levels between {df_levels!r} and {gr_levels!r}")
-    grouping_df = pd.DataFrame(index=grouping)
-    df_aligned = grouping_df.join(
-        df,
-        how="left",
-    )
+    if df.columns.nlevels > 1:
+        column_index = pd.MultiIndex.from_tuples(df.columns, names=df.columns.names)
+        grouping_df = pd.DataFrame(index=grouping, columns=column_index)
+        df_aligned = grouping_df.join(df, how="left", lsuffix="_")
+    else:
+        grouping_df = pd.DataFrame(index=grouping)
+        df_aligned = grouping_df.join(
+            df,
+            how="left",
+        )
     level_order = gr_levels + [level for level in df_levels if level not in gr_levels]
     result = df_aligned.reorder_levels(level_order)
     if sort_index:
@@ -841,6 +846,8 @@ def make_index_from_grouping_dict(
     Returns:
         A MultiIndex with the given names and the tuples from the grouping dictionary.
     """
+    if not isinstance(grouping, dict):
+        raise TypeError(f"grouping must be a dict, not {type(grouping)}")
     if raise_if_multiple_membership:
         all_members = []
         for members in grouping.values():
